de402fa200bea6b5ccc1c9ee417e19371f483bb6
Leon Molina <leon.maguiar@gmail.com>
2022-03-18 17:02:23 -0300

    added travis ci

--- src/components/Header/HeaderStyles.ts
+++ src/components/Header/HeaderStyles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components/macro';
+import styled from 'styled-components';
 
 export const NavBar = styled.div`
   height: 80px;
--- src/components/partials/ProjectsSection/ProjectsSectionStyles.ts
+++ src/components/partials/ProjectsSection/ProjectsSectionStyles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components/macro';
+import styled from 'styled-components';
 
 export const ProjectsSection = styled.section`
   height: 500px;
