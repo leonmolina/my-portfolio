f3d393e3bc6a0c3a247b0f5ef431f2893b7f098c
Leon Molina <leon.maguiar@gmail.com>
2022-03-18 16:00:06 -0300

    trying to fix pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,24 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
+// export default defineConfig({
+//   plugins: [react()]
+// })
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          [
+            'babel-plugin-styled-components',
+            {
+              displayName: true,
+              fileName: false
+            }
+          ]
+        ]
+      }
+    })
+  ]
 })
