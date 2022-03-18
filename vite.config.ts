13ae6a615132613b8076e276201233c3cb51d4e1
Leon Molina <leon.maguiar@gmail.com>
2022-03-18 16:08:48 -0300

    setting up vite babel macro

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import macrosPlugin from 'vite-plugin-babel-macros'
 
 // https://vitejs.dev/config/
-
-// export default defineConfig({
-//   plugins: [react()]
-// })
 export default defineConfig({
   plugins: [
-    react({
-      babel: {
-        plugins: [
-          [
-            'babel-plugin-styled-components',
-            {
-              displayName: true,
-              fileName: false
-            }
-          ]
-        ]
-      }
-    })
+    react(),
+    macrosPlugin()
   ]
 })
